7828c45ac4318788fe3ce4dcae342866dcfbb5f1
Wray Zheng <wrayzheng@outlook.com>
2018-09-20 15:35:31 +0800

    exercise: error-handling

---
+++ error-handling/src/main/java/ErrorHandling.java
@@ -0,0 +1,54 @@
+import java.io.IOException;
+import java.util.Optional;
+
+class ErrorHandling {
+
+    void handleErrorByThrowingIllegalArgumentException() {
+        throw new IllegalArgumentException();
+    }
+
+    void handleErrorByThrowingIllegalArgumentExceptionWithDetailMessage(String message) {
+        throw new IllegalArgumentException(message);
+    }
+
+    void handleErrorByThrowingAnyCheckedException() throws IOException {
+        throw new IOException();
+    }
+
+    void handleErrorByThrowingAnyCheckedExceptionWithDetailMessage(String message) throws IOException {
+        throw new IOException(message);
+    }
+
+    void handleErrorByThrowingAnyUncheckedException() {
+        throw new IndexOutOfBoundsException();
+    }
+
+    void handleErrorByThrowingAnyUncheckedExceptionWithDetailMessage(String message) {
+        throw new IndexOutOfBoundsException(message);
+    }
+
+    void handleErrorByThrowingCustomCheckedException() throws CustomCheckedException {
+        throw new CustomCheckedException();
+    }
+
+    void handleErrorByThrowingCustomCheckedExceptionWithDetailMessage(String message) throws CustomCheckedException {
+        throw new CustomCheckedException(message);
+    }
+
+    void handleErrorByThrowingCustomUncheckedException() {
+        throw new CustomUncheckedException();
+    }
+
+    void handleErrorByThrowingCustomUncheckedExceptionWithDetailMessage(String message) {
+        throw new CustomUncheckedException(message);
+    }
+
+    Optional<Integer> handleErrorByReturningOptionalInstance(String integer) {
+        try {
+            return Optional.of(Integer.valueOf(integer));
+        } catch (NumberFormatException e) {
+            return Optional.empty();
+        }
+    }
+
+}
